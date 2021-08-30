cd3ef335f046149a121d70bae064341d6967d83e
dean <diazdean21@gmail.com>
2021-08-29 21:06:00 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,events) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+    messages.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(positiveNumbers) {
+    while (positiveNumbers >= 0) {
+        console.log(positiveNumbers );
+        positiveNumbers --;
+    }
+}
