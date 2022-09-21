333e0ac95039700b037b62b734c24f73cef0b8e6
dean <diazdean21@gmail.com>
2022-09-20 20:00:17 -0500

    commit tree 72cfbff4c8f2763e9bf81b3fbb6700a9df34217e

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 // Code your solutions in this file
-function writeCards(names,events) {
-    let messages = [];
-    for (let i = 0; i < names.length; i++) {
-    messages.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-    }
-    return messages;
+
+// function writeCards(names, events) {
+//   let messages = [];
+//   for (let i = 0; i < names.length; i++) {
+//     messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+//   }
+//   return messages;
+// }
+
+// function countDown(positiveNumbers) {
+//   while (positiveNumbers >= 0) {
+//     console.log(positiveNumbers);
+//     positiveNumbers--;
+//   }
+// }
+
+function writeCards(names, events) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return messages;
 }
 
 function countDown(positiveNumbers) {
-    while (positiveNumbers >= 0) {
-        console.log(positiveNumbers );
-        positiveNumbers --;
-    }
+  while (positiveNumbers >= 0) {
+    console.log(positiveNumbers);
+    positiveNumbers--;
+  }
 }
